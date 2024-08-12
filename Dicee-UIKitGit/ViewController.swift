499fa9023aa6b71f41d04be621b1e847fd9a0938
allaios <co.alla.ios@gmail.com>
2024-08-12 10:19:39 -0700

    New DiceeeApp

--- Dicee-UIKitGit/ViewController.swift
+++ Dicee-UIKitGit/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var diceeImage1: UIImageView!
+    @IBOutlet weak var diceeImage2: UIImageView!
+    
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceeImage1.image = diceArray.randomElement()
+        diceeImage2.image = diceArray.randomElement()
+        
     }
-
-
+    
 }
 
